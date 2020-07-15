90b35b8278cc6df89af0aa2bacba560ed0d493c9
simar <simar1992@gmail.com>
2020-07-15 19:12:25 +0200

    Updates

---
+++ precache-manifest.44238ed0e3aa133b489f88a54e60e1e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eac6caca56dba6de5f939f7aac3ef59",
+    "url": "/onlineshopping/index.html"
+  },
+  {
+    "revision": "7c66d5975c0c21a7b53c",
+    "url": "/onlineshopping/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "abaeb3769486d1bfcd2e",
+    "url": "/onlineshopping/static/css/main.10811484.chunk.css"
+  },
+  {
+    "revision": "7c66d5975c0c21a7b53c",
+    "url": "/onlineshopping/static/js/2.e121897b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/onlineshopping/static/js/2.e121897b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abaeb3769486d1bfcd2e",
+    "url": "/onlineshopping/static/js/main.6fe63fca.chunk.js"
+  },
+  {
+    "revision": "5a5d433d76215b11b4bd",
+    "url": "/onlineshopping/static/js/runtime-main.047b20f7.js"
+  },
+  {
+    "revision": "241355bd29e72781add928688adfdd35",
+    "url": "/onlineshopping/static/media/background3.241355bd.jpg"
+  }
+]);
